51156d5e5e2deaaee2f4b10e7b200bdeab006463
Jonah Ringdahl <JonahRingdahl@outlook.com>
2024-06-25 11:24:45 -0400

    Default template building correctly

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_CSharp.Windowing;
+using Raylib_CSharp.Rendering;
+using Raylib_CSharp.Colors;
+
+Window.Init(800,480, "Hello World");
+
+while (!Window.ShouldClose())
+{
+    Graphics.BeginDrawing();
+    Graphics.ClearBackground(Color.White);
+
+    Graphics.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+    Graphics.EndDrawing();
+}
+
+Window.Close();
