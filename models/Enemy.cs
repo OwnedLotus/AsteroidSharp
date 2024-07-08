d06508308ffa44b26ba49546d3f206dcd1c2f5f2
Jonah Ringdahl <jonahringdahl@outlook.com>
2024-07-07 22:00:09 -0400

    adding some enemy functionallity

--- models/Enemy.cs
+++ models/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using AsteroidSharp.Models.Shapes;
+using Raylib_CSharp.Colors;
 
 
 namespace AsteroidSharp.Models;
@@ -10,6 +11,7 @@ class Enemy(Vector2 StartingPosition, Vector2 localDirection, float s)
     private Vector2 _heading = localDirection;
     private IShape _shape = new EnemyShape();
     private float _speed = s;
+    private Queue<Bullet> _bullets = new Queue<Bullet>();
 
 
     public void DrawEnemy()
@@ -19,10 +21,10 @@ class Enemy(Vector2 StartingPosition, Vector2 localDirection, float s)
 
     public void MoveEnemy()
     {
-        throw new NotImplementedException();
+        _pos += _heading * _speed;
     }
     public void EnemyShoot()
     {
-        throw new NotImplementedException();
+        _bullets.Enqueue(new Bullet(new BulletShape(5, _heading), _pos, _heading, Color.Red, 2));
     }
 }
