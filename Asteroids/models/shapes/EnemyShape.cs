7f77c43f355ea12ad99fd4fa3a6a9a8f0462db8a
Jonah Ringdahl <JonahRingdahl@outlook.com>
2024-07-26 11:45:37 -0400

    add some functionality to enemyshape

--- Asteroids/models/shapes/EnemyShape.cs
+++ Asteroids/models/shapes/EnemyShape.cs
@@ -1,26 +1,32 @@
 using System.Numerics;
 using Raylib_CSharp.Colors;
+using Raylib_CSharp.Rendering;
 
 namespace AsteroidSharp.Models.Shapes;
 
 
 class EnemyShape : IShape
 {
+    private Vector2 _pos;
+    private Vector2 _bounds;
     private Color _color;
+
 
     public Vector2[] Corners { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     public Color ShapeColor { get => _color; set => _color = value; }
 
-    public Raylib_CSharp.Transformations.Rectangle Rectangle => throw new NotImplementedException();
-
-    public EnemyShape()
+    public EnemyShape(Vector2 bounds, Color color, Vector2 pos)
     {
-
+        _bounds = bounds;
+        _color = color;
+        _pos = pos;
     }
 
     public Vector2 UpdateShape(Vector2 position)
     {
-        throw new NotImplementedException();
+        _pos = position;
+
+        return Vector2.Normalize(_pos);
     }
 
     public void DrawShape()
@@ -28,13 +34,13 @@ class EnemyShape : IShape
         // I was thinking to have the enemies as ovals if I am not going to implement
         // them as actual meshes and render an actual image
 
-        throw new NotImplementedException();
-
+        Graphics.DrawEllipseLines((int)_pos.X, (int)_pos.Y, (int)_bounds.X, (int)_bounds.Y, _color);
     }
 
     public Vector2 RotateShape(Vector2 position, float rotation)
     {
-        throw new NotImplementedException();
+        // I think that all enemy movement will be upright
+        return Vector2.Zero;
     }
 
     public bool Collision(IEnumerable<Vector2> boundaries)
